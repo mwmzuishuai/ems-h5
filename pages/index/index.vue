7f3bb09b9808a4921e02028f925ad0425b43a5e1
mwmzuishuai <2713459649@qq.com>
2024-08-07 15:51:35 +0800

    状态管理库添加

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,37 +1,30 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<!-- <image class="logo" src="/static/logo.png"></image> -->
 			<view style="padding: 20px;">
 				<up-button type="primary" text="确定"></up-button>
 				<up-button type="primary" :plain="true" text="镂空"></up-button>
 				<up-button type="primary" :plain="true" :hairline="true" text="细边"></up-button>
-				<up-button type="primary" :disabled="disabled" text="禁用"></up-button>
+				<up-button type="primary" disabled text="禁用"></up-button>
 				<up-button type="primary" loading loadingText="加载中"></up-button>
 				<up-button type="primary" icon="map" text="图标按钮"></up-button>
 				<up-button type="primary" shape="circle" text="按钮形状"></up-button>
 				<up-button text="渐变色按钮" color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"></up-button>
-				<up-button type="primary" size="small" text="大小尺寸"></up-button>
+				<up-button type="primary" size="small" text="大小尺寸" @click="upNum()"></up-button>
+			</view>
+			<view>
+				{{num}}
 			</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+<script setup>
+	import { ref } from 'vue';
+	import {useTest} from "@/store/test.js"
+	import {storeToRefs} from 'pinia'
+	const test = useTest()
+	const {num} = storeToRefs(test)
+	const {upNum} =test
 </script>
 
 <style>
